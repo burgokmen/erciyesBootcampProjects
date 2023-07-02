8fadbbd5af1257acaecd08bb78947752c23c5d2e
burgokmen <burgokmen@gmail.com>
2023-07-02 14:57:05 +0300

    2 Projects done 11 ongoing

--- 1-HelloWorld/HelloWorld2.js
+++ 1-HelloWorld/HelloWorld2.js
@@ -1,48 +1,47 @@
-import React from 'react'
-import { SafeAreaView, Text, StyleSheet } from 'react-native'
+import React from "react";
+import { SafeAreaView, Text, StyleSheet } from "react-native";
 import Constants from "expo-constants";
 
 export default function HelloWorld2() {
   return (
     <SafeAreaView>
-    <Text style={styles.container}>
+      <Text style={styles.container}>
         Hello <Text style={styles.highlightText1}>World</Text>
-    </Text>
+      </Text>
 
-    <Text style={styles.heading}>
+      <Text style={styles.heading}>
         Hello <Text style={styles.highlightText2}>World</Text>
-    </Text>
+      </Text>
 
-    <Text style={[styles.heading, {color:"#5959d"}]}>
+      <Text style={[styles.heading, { color: "#5959d" }]}>
         Hello <Text style={styles.highlightText2}>World</Text>
-    </Text>
-
+      </Text>
     </SafeAreaView>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-    container:{
+  container: {
     //SafeAreaView on Android devices
     paddingTop: Constants.statusBarHeight,
-    },
-    heading:{
-        marginTop: 50,
-        textAlign: "center",
-        fontSize: 30,
-        fontWeight: "bold",
-        textTransform: "uppercase",
-    },
-    highlightText1: {
-        color:"#e74c3c",
-    },
-    highlightText2: {
-        fontStyle: "italic",
-        TextDecorationLine: "underline",
-        color:"#2980b9",
-    },
-    highlightText3: {
-        color:"#fff",
-        backgroundColor: "#59595d"
-    }
-})
+  },
+  heading: {
+    marginTop: 50,
+    textAlign: "center",
+    fontSize: 30,
+    fontWeight: "bold",
+    textTransform: "uppercase",
+  },
+  highlightText1: {
+    color: "#e74c3c",
+  },
+  highlightText2: {
+    fontStyle: "italic",
+    TextDecorationLine: "underline",
+    color: "#2980b9",
+  },
+  highlightText3: {
+    color: "#fff",
+    backgroundColor: "#59595d",
+  },
+});
